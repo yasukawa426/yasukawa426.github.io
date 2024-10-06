f8f2f01f55cef7d901516bbd4fca07b5fb8db27b
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-10-06 19:16:39 -0300

    Fixed <a> links

--- random_stuff/html_indexer.py
+++ random_stuff/html_indexer.py
@@ -33,7 +33,7 @@ def generate_html_list(path, number=0):
                 # Remove "_", make each word capitalized, and turns ".Html" into ".html"
                 formated_item = item.replace("_", " ").title().replace("H", "h")
 
-                html += f'<li><a href="{os.path.relpath(item_path, path)}">{formated_item}:</a></li>\n'  # File
+                html += f'<li><a href="{item_path}">{formated_item}</a></li>\n'  # File
     html += "</ul>\n"
 
     # returns a tuple with the html and number of pages
