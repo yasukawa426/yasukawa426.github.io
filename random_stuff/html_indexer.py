d2035d7c913793ea5950105085ef8892e4f3583f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-10-19 04:35:47 -0300

    Added class to generated elements.

--- random_stuff/html_indexer.py
+++ random_stuff/html_indexer.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import sys
 
+
 # Recursively generate HTML list items for directory structure.
 # path is the path we are listing. you should call using the root you want to analyze.
 # number is the number of pages (.html) we currently are counting. Used when being called recursevely.
@@ -15,7 +16,7 @@ def generate_html_list(path, number=0):
             # Remover "_" and make each word capitalized.
             formated_item = item.replace("_", " ").title()
 
-            html += f"<li><strong>{formated_item}:</strong></li>\n"  # Directory
+            html += f"<li class='folder'><strong>{formated_item}:</strong></li>\n"  # Directory
 
             results = generate_html_list(
                 item_path, page_number
@@ -35,7 +36,7 @@ def generate_html_list(path, number=0):
                 # Remove "_", make each word capitalized, and turns ".Html" into ".html"
                 formated_item = item.replace("_", " ").replace(".html", "").title()
 
-                html += f'<li><a href="{item_path}">{formated_item}</a></li>\n'  # File
+                html += f'<li class="file"><a href="{item_path}">{formated_item}</a></li>\n'  # File
     html += "</ul>\n"
 
     # returns a tuple with the html and number of pages
@@ -49,7 +50,7 @@ if __name__ == "__main__":
 
     # incresing recursio limit
     # sys.setrecursionlimit(5000)
-    
+
     html, pages = generate_html_list(HTML_PATH)
     html = f"<p>Number of available pages: {pages}</p> \n{html}"
 
